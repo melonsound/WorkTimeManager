6a0c18430de88b5c95c6b84991a32939a38066bf
melonsound <ghost.134@hotmail.com>
2021-08-28 18:44:11 +0300

    feat: модель задач и подзадач

---
+++ WorkTimeManager.Api/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkTimeManager.Api.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public List<Subtask> Subtasks { get; set; }
+
+    }
+}
