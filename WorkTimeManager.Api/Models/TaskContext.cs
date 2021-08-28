95a19f60f1c6148ffd905c54296ce4ad4c43a736
melonsound <ghost.134@hotmail.com>
2021-08-28 19:10:34 +0300

    feat: связка модели с базой

---
+++ WorkTimeManager.Api/Models/TaskContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkTimeManager.Api.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Subtask> Subtasks { get; set; }
+    }
+}
