709fbe44f6c8489ce08063d975f65b122b60e588
melonsound <ghost.134@hotmail.com>
2022-01-15 16:04:12 +0300

    add namespace reference to dto

--- src/WorkTimeManager.Core/Interfaces/ITaskService.cs
+++ src/WorkTimeManager.Core/Interfaces/ITaskService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkTimeManager.Core.Models.Dto;
 
 namespace WorkTimeManager.Core.Interfaces
 {
