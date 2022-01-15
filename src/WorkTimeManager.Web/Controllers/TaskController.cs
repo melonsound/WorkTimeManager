9e7100774bb8b66ccec5bac579f1e928ef01f541
melonsound <ghost.134@hotmail.com>
2022-01-15 18:16:49 +0300

    add ui

---
+++ src/WorkTimeManager.Web/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkTimeManager.Core.Interfaces;
+using WorkTimeManager.Core.Models.Dto;
+using WorkTimeManager.Web.Controllers.Base;
+
+namespace WorkTimeManager.Web.Controllers
+{
+    public class TaskController : ApiController
+    {
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpPost("create")]
+        public async Task<TaskEntityDto> CreateTask([FromBody] TaskEntityDto taskEntityDto)
+        {
+            var result = await _taskService.CreateTaskAsync(taskEntityDto);
+            if (result == null)
+                throw new Exception("Error");
+
+            return result;
+        }
+    }
+}
---
+++ src/WorkTimeManager.Web/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using WorkTimeManager.Core.Interfaces;
+using WorkTimeManager.Infrastructure;
+using WorkTimeManager.Infrastructure.Data;
+using WorkTimeManager.Infrastructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
+var connString = builder.Configuration["MainApp:AppDbConnectionString"];
+builder.Services.AddDbContext(connString);
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddScoped<ITaskService, TaskService>();
+
+var app = builder.Build();
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var appDataDbContext = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+    appDataDbContext.Database.Migrate();
+}
+
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
