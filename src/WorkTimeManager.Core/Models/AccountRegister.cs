4451b1297cf650328aa18c3fe5dd50b84d9ce258
melonsound <ghost.134@hotmail.com>
2022-02-05 14:36:17 +0300

    add security core models

---
+++ src/WorkTimeManager.Core/Models/AccountRegister.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTimeManager.Core.Models
+{
+    public class AccountRegister
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
