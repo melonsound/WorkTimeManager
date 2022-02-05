84cae8b53301f950921398e7a64ed33e3fffc602
melonsound <ghost.134@hotmail.com>
2022-02-05 14:39:38 +0300

    DI container

--- src/WorkTimeManager.Web/Program.cs
+++ src/WorkTimeManager.Web/Program.cs
@@ -1,27 +1,53 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WorkTimeManager.Core.Interfaces;
 using WorkTimeManager.Infrastructure;
 using WorkTimeManager.Infrastructure.Data;
 using WorkTimeManager.Infrastructure.Services;
+using WorkTimeManager.Security;
+using WorkTimeManager.Security.Data;
+using WorkTimeManager.Security.Models;
+using WorkTimeManager.Security.Validatiors;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration["MainApp:LocalDb"];
+var securityConnString = builder.Configuration["MainApp:LocalDbSecurity"];
+
+// #Startup
+#region Services
 
 builder.Services.AddControllers();
 
 builder.Services.AddSwaggerGen();
-
-var connString = builder.Configuration["MainApp:AppDbConnectionString"];
+builder.Services.AddSecurityDbContext(securityConnString);
 builder.Services.AddDbContext(connString);
+builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<IValidator<AppUser>, AppUserValidator>();
+
+builder.Services.AddIdentity<AppUser, AppUserRole>(options =>
+{
+    builder.Configuration.GetSection("IdentityOptions").Bind(options);
+})
+.AddEntityFrameworkStores<AppSecurityDbContext>()
+.AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider);
+
+#endregion
+
+
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-builder.Services.AddScoped<ITaskService, TaskService>();
 
 var app = builder.Build();
 
 using (var serviceScope = app.Services.CreateScope())
 {
     var appDataDbContext = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var appSecurityDataDbContext = serviceScope.ServiceProvider.GetRequiredService<AppSecurityDbContext>();
     appDataDbContext.Database.Migrate();
+    appSecurityDataDbContext.Database.Migrate();
 }
 
 app.UseRouting();
