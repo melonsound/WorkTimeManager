288780d33c6d9335e0ca298c70435b57a3842f5b
melonsound <ghost.134@hotmail.com>
2022-02-05 14:38:17 +0300

    add fluent validation

---
+++ src/WorkTimeManager.Security/Validatiors/ErrorMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTimeManager.Security.Validatiors
+{
+    public static class ErrorMessage
+    {
+        public static string PasswordLength(int length) => $"Пароль должен содержать минимум {length} символов";
+        public static string PasswordUppercase => $"Пароль должен содержать буквы верхнего регистра.";
+        public static string PasswordLowercase => $"Пароль должен содержать буквы нижнего регистра.";
+        public static string PasswordDigit => $"Пароль должен содержать цифры.";
+        public static string UsernameLength(int length) => $"Логин должен содержать минимум {length} символов";
+    }
+}
