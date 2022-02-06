02383c9d67d70bbb674ea44995f1f7616f80ca1a
melonsound <ghost.134@hotmail.com>
2022-02-06 20:32:28 +0300

    add roles controller

--- src/WorkTimeManager.Web/Controllers/AccountController.cs
+++ src/WorkTimeManager.Web/Controllers/AccountController.cs
@@ -1,73 +1,34 @@
-﻿using FluentValidation;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WorkTimeManager.Core.Interfaces;
 using WorkTimeManager.Core.Models;
-using WorkTimeManager.Security.Models;
 using WorkTimeManager.Web.Controllers.Base;
 
 namespace WorkTimeManager.Web.Controllers
 {
     public class AccountController : ApiController
     {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly IValidator<AppUser> _validator;
+        private readonly IAccountService _accountService;
 
-        public AccountController(UserManager<AppUser> userManager,
-                                 IValidator<AppUser> validator)
+        public AccountController(IAccountService accountService)
         {
-            _userManager = userManager;
-            _validator = validator;
+            _accountService = accountService;
         }
 
+        [AllowAnonymous]
         [HttpPost("register")]
-        public async Task<Response> Register(AccountRegister account)
+        public async Task<Response> Register([FromBody]AccountRegister account)
         {
-            var accountResult = await _userManager.FindByNameAsync(account.Username);
-            var response = new Response();
-
-            if (accountResult != null)
-            {
-                response.Success = false;
-                response.Message = "User already registered";
-                return response;
-            }
-
-            var userForValidate = new AppUser()
-            {
-                UserName = account.Username,
-                PasswordHash = account.Password
-            };
-
-            var validatorResult = await _validator.ValidateAsync(userForValidate);
-            if (!validatorResult.IsValid)
-            {
-                response.Success = false;
-                response.Message = validatorResult.ToString();
-                return response;
-            }
-
-            var newAccount = new AppUser()
-            {
-                UserName = account.Username
-            };
-
-
-            var regResult = await _userManager.CreateAsync(newAccount, account.Password);
-            if (regResult.Succeeded)
-            {
-                response.Success = true;
-                response.Message = regResult.ToString();
-                return response;
-            }
-
-            var errorText = regResult.Errors.Aggregate("Ошибка при регистрации пользователя. Errors: \n\r\n\r",
-                (current, error) => current + (" - " + error.Description + "\n\r"));
-
-            response.Success = false;
-            response.Message = errorText;
-            return response;
+            var registerResult = await _accountService.RegisterUserAsync(account);
+            return registerResult;
         }
 
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<Response> Login([FromBody]AccountRegister account)
+        {
+            var loginResult = await _accountService.LoginUserAsync(account);
+            return loginResult;
+        }
     }
 }
