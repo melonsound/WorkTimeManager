118acc58bf366ebe2afecf469bfb33c14181408f
melonsound <ghost.134@hotmail.com>
2022-02-06 20:33:33 +0300

    add jwt bearer authentication

--- src/WorkTimeManager.Web/Program.cs
+++ src/WorkTimeManager.Web/Program.cs
@@ -1,7 +1,9 @@
 using FluentValidation;
-using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WorkTimeManager.Core.Interfaces;
 using WorkTimeManager.Infrastructure;
 using WorkTimeManager.Infrastructure.Data;
@@ -9,23 +11,26 @@ using WorkTimeManager.Infrastructure.Services;
 using WorkTimeManager.Security;
 using WorkTimeManager.Security.Data;
 using WorkTimeManager.Security.Models;
+using WorkTimeManager.Security.Services;
 using WorkTimeManager.Security.Validatiors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connString = builder.Configuration["MainApp:LocalDb"];
 var securityConnString = builder.Configuration["MainApp:LocalDbSecurity"];
+var key = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("JWT_SECURITY_KEY"));
 
 // #Startup
 #region Services
 
 builder.Services.AddControllers();
-
+builder.Services.AddRouting(x => x.LowercaseUrls = true);
 builder.Services.AddSwaggerGen();
 builder.Services.AddSecurityDbContext(securityConnString);
 builder.Services.AddDbContext(connString);
 builder.Services.AddScoped<ITaskService, TaskService>();
 builder.Services.AddScoped<IValidator<AppUser>, AppUserValidator>();
+builder.Services.AddScoped<IAccountService, AccountService>();
 
 builder.Services.AddIdentity<AppUser, AppUserRole>(options =>
 {
@@ -33,6 +38,26 @@ builder.Services.AddIdentity<AppUser, AppUserRole>(options =>
 })
 .AddEntityFrameworkStores<AppSecurityDbContext>()
 .AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false
+        
+    };
+});
 
 #endregion
 
@@ -53,6 +78,9 @@ using (var serviceScope = app.Services.CreateScope())
 app.UseRouting();
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
