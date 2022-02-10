0c18486c7775321aa060b774dc27b8bfab4ae35d
melonsound <ghost.134@hotmail.com>
2022-02-10 22:29:12 +0300

    task entity validator

---
+++ src/WorkTimeManager.Infrastructure/Validators/TaskEntityValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkTimeManager.Core.Models;
+
+namespace WorkTimeManager.Infrastructure.Validators
+{
+    public class TaskEntityValidator : AbstractValidator<TaskEntity>
+    {
+        public TaskEntityValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage(TaskErrorMessage.NullOrEmpty)
+                .NotNull().WithMessage(TaskErrorMessage.NullOrEmpty);
+
+
+        }
+    }
+}
---
+++ src/WorkTimeManager.Infrastructure/Validators/TaskErrorMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTimeManager.Infrastructure.Validators
+{
+    public static class TaskErrorMessage
+    {
+        public static string NullOrEmpty { get; set; } = "Название не может быть пустым!";
+
+    }
+}
