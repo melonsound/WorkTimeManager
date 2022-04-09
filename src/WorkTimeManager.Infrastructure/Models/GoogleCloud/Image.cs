80dc9f337f678e1ce4c4be44680787b529c1001e
melonsound <ghost.134@hotmail.com>
2022-04-09 15:15:33 +0300

    google cloud support

---
+++ src/WorkTimeManager.Infrastructure/Models/GoogleCloud/Image.cs
@@ -0,0 +1,9 @@
+﻿using WorkTimeManager.Infrastructure.Models.GoogleCloud.Base;
+
+namespace WorkTimeManager.Infrastructure.Models.GoogleCloud
+{
+    public class Image : GoogleCloudFile
+    {
+        public string WebContentLink { get; set; } = string.Empty;
+    }
+}
